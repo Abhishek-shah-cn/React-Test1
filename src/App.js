fc92fe7e4021f6fa62e5b4c04e460569582609b2
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2020-08-09 20:17:09 +0530

    This is toll tip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ToolTips from "./ToolTips";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      top: true,
+      left: false,
+      right: false,
+      bottom: false,
+    };
+  }
+
+  changeStatetop = () => {
+    console.log("Insidee top");
+    this.setState({ top: true, left: false, right: false, bottom: false });
+  };
+
+  changeStateleft = () => {
+    this.setState({ top: false, left: true, right: false, bottom: false });
+  };
+
+  changeStateright = () => {
+    this.setState({ top: false, left: false, right: true, bottom: false });
+  };
+
+  changeStatebottom = () => {
+    this.setState({ top: false, left: false, right: false, bottom: true });
+  };
+
+  render() {
+    const { top, left, right, bottom } = this.state;
+    return (
+      <div>
+        <div className="Text">Click On DropDown Button---🔻</div>
+        <div className="dropdown">
+          <button className="dropbtn" onClick={this.changestate}>
+            Dropdown
+          </button>
+          <div className="dropdown-content">
+            <ul className="dropdown-menu">
+              <li className="li" onClick={this.changeStateleft}>
+                {" "}
+                left{" "}
+              </li>
+              <li className="li" onClick={this.changeStateright}>
+                {" "}
+                right{" "}
+              </li>
+              <li className="li" onClick={this.changeStatetop}>
+                {" "}
+                top{" "}
+              </li>
+              <li className="li" onClick={this.changeStatebottom}>
+                {" "}
+                bottom{" "}
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {top && <ToolTips content="this is for ☝️" addClass="top" />}
+        {left && <ToolTips content="this is for 👈" addClass="left" />}
+        {right && <ToolTips content="this is for 👉" addClass="right" />}
+        {bottom && <ToolTips content="this is for 👇" addClass="bottom" />}
+      </div>
+    );
+  }
 }
-
-export default App;
